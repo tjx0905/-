a6b7569d0ffe49866e8e76e439e90e1346faaa5e
唐俊祥 <tjx1976259749@163.com>
2019-08-13 17:47:49 +0800

    bug

---
+++ src/components/navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="tab-bar">
+    <van-tabbar v-model="active">
+      <van-tabbar-item name="home" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item name="energy" icon="search">能耗</van-tabbar-item>
+      <van-tabbar-item name="facility" icon="friends-o">设备</van-tabbar-item>
+      <van-tabbar-item name="environment" icon="setting-o">环境</van-tabbar-item>
+      <van-tabbar-item name="order" icon="setting-o">工单</van-tabbar-item>
+      <van-tabbar-item name="polling" icon="setting-o">巡检</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+
+<script>
+import Vue from "vue";
+import Button from "vant/lib/button";
+import "vant/lib/button/style";
+import { Tabbar, TabbarItem } from "vant";
+Vue.use(Tabbar).use(TabbarItem);
+export default {
+  // props: {
+  //   courrentTabBar: {
+  //     type: String
+  //   }
+  // },
+  // name: "home",
+  components: {
+    // HelloWorld
+    // navbar
+  },
+  data() {
+    return {
+      active: 'home'
+    }
+  },
+  methods: {
+   
+  },
+  mounted() {
+
+  }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <navbar/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import navbar from '@/components/navbar.vue'
+
+export default {
+  name: 'home',
+  components: {
+    navbar
+  }
+}
+</script>
